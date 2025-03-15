5f20c47a25cc0d07e407174e4d6cd385cb5bc153
Tal Svorai <tsavorai@bmc.com>
2025-03-15 20:31:55 +0200

    allowing ssh access

--- song_terraform/main.tf
+++ song_terraform/main.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "allow_ports" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow access from anywhere
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
